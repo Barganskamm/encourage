a22e4575446d18b290c591cd2248ba458a77b2ba
Barganskamm <barganskamm@gmail.com>
2020-06-26 10:18:09 +0200

    Little encouragement

---
+++ index.js
@@ -0,0 +1,2 @@
+const message = "'The truth is that our finest moments are most likely to occur when we are feeling deeply uncomfortable, unhappy, or unfulfilled. For it is only in such moments, propelled by our discomfort, that we are likely to step out of our ruts and start searching for different ways or truer answers.' ~ M.Scott Peck";
+console.log(message);
